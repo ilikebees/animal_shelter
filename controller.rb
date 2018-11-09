60fbc1973e8fabbb83172f19bdc39d486c74c95c
Laura Manson <hellolauramanson@gmail.com>
2018-11-09 16:42:28 +0000

    added a homepage

--- controller.rb
+++ controller.rb
@@ -1,9 +1,19 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require_relative('models/animal')
+require_relative('models/owner')
 also_reload('./models/*')
 
 get '/animals' do
   @animals = Animal.all
   erb(:index)
+end
+
+# get '/owners' do
+#   @owners = Owner.all
+#   erb(:index)
+# end
+
+get '/home' do
+  erb(:home)
 end
